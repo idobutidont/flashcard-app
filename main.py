69c434c01653fd11983b5866c31c0f7ead93a288
Idotoho Reimon Simanjuntak <106871003+idobutidont@users.noreply.github.com>
2025-03-17 12:17:48 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
+import os
 from PyQt6.QtWidgets import QApplication
 from Virli_241524062 import FlashcardApp
 
 if __name__ == "__main__":
+    # Create ./data directory if it doesn't exist yet
+    os.makedirs("data", exist_ok=True)
+    
     app = QApplication(sys.argv)
     window = FlashcardApp()
     window.show()
